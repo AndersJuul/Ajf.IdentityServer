7bb616cd4dea9a7007e697fc82e7542e7386312c
Anders Juul <anders_privat@hotmail.com>
2017-06-30 20:56:12 +0200

    a

--- src/Ajf.IdentityServer/Startup.cs
+++ src/Ajf.IdentityServer/Startup.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using IdentityServer4.Test;
-using IdentityServer4.Models;
-using System.Security.Claims;
-using IdentityModel;
 
 namespace Ajf.IdentityServer
 {
@@ -40,76 +32,7 @@ namespace Ajf.IdentityServer
 
             app.UseIdentityServer();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
-    }
-    internal class Resources
-    {
-        public static IEnumerable<IdentityResource> GetIdentityResources()
-        {
-            return new List<IdentityResource> {
-            new IdentityResources.OpenId(),
-            new IdentityResources.Profile(),
-            new IdentityResources.Email(),
-            new IdentityResource {
-                Name = "role",
-                UserClaims = new List<string> {"role"}
-            }
-        };
-        }
-
-        public static IEnumerable<ApiResource> GetApiResources()
-        {
-            return new List<ApiResource> {
-            new ApiResource {
-                Name = "customAPI",
-                DisplayName = "Custom API",
-                Description = "Custom API Access",
-                UserClaims = new List<string> {"role"},
-                ApiSecrets = new List<Secret> {new Secret("scopeSecret".Sha256())},
-                Scopes = new List<Scope> {
-                    new Scope("customAPI.read"),
-                    new Scope("customAPI.write")
-                }
-            }
-        };
-        }
-        }
-    internal class Users
-    {
-        public static List<TestUser> Get()
-        {
-            return new List<TestUser> {
-            new TestUser {
-                SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
-                Username = "scott",
-                Password = "password",
-                Claims = new List<Claim> {
-                    new Claim(JwtClaimTypes.Email, "scott@scottbrady91.com"),
-                    new Claim(JwtClaimTypes.Role, "admin")
-                }
-            }
-        };
-        }
-    }
-
-    internal class Clients
-    {
-        public static IEnumerable<Client> Get()
-        {
-            return new List<Client> {
-            new Client {
-                ClientId = "oauthClient",
-                ClientName = "Example Client Credentials Client Application",
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets = new List<Secret> {
-                    new Secret("superSecretPassword".Sha256())},
-                AllowedScopes = new List<string> {"customAPI.read"}
-            }
-        };
+            app.Run(async context => { await context.Response.WriteAsync("Hello World!"); });
         }
     }
 }
