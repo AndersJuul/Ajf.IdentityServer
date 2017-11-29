bdc7314f25225ac8e3ba5146bcc13fcb63d83394
AndersJuul <andersjuulsfirma@gmail.com>
2017-11-29 20:05:49 +0100

    a

--- src/TripGallery.Constants/Constants.cs
+++ src/TripGallery.Constants/Constants.cs
@@ -11,7 +11,7 @@ namespace TripGallery
 
         public const string TripGalleryClientSecret = "myrandomclientsecret";
 
-        public const string TripGalleryIssuerUri = "https://tripcompanysts/identity";
+        public const string TripGalleryIssuerUri = "https://andersathome.dk/identity";
         public const string TripGallerySTSOrigin = "https://localhost:44317";
         public const string TripGallerySTS = TripGallerySTSOrigin + "/identity";
         public const string TripGallerySTSTokenEndpoint = TripGallerySTS + "/connect/token";
