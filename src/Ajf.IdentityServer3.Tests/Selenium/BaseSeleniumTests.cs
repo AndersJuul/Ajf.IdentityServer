5048dafa3a9bc059e5946bae28e97a9791cb27cc
AndersJuul <andersjuulsfirma@gmail.com>
2017-12-03 15:11:38 +0100

    a

--- src/Ajf.IdentityServer3.Tests/Selenium/BaseSeleniumTests.cs
+++ src/Ajf.IdentityServer3.Tests/Selenium/BaseSeleniumTests.cs
@@ -10,32 +10,18 @@ namespace Ajf.IdentityServer3.Tests.Selenium
 {
     public class BaseSeleniumTests
     {
-        private Process _iisProcess;
         public RemoteWebDriver ChromeDriver { get; set; }
 
         public Uri BaseUri { get; set; }
 
-        private void StartIIS()
-        {
-            var applicationPath =Path.GetFullPath(@"C:\Projects\Ajf.IdentityServer\src\Ajf.IdentityServer3");
-            var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
-            _iisProcess = new Process();
-            _iisProcess.StartInfo.FileName = programFiles + @"\IIS Express\iisexpress.exe";
-            _iisProcess.StartInfo.Arguments = string.Format("/path:\"{0}\" /port:{1}", applicationPath, 44317 );
-            _iisProcess.Start();
-        }
 
         [SetUp]
         public void SetUp()
         {
-            if (ConfigurationManager.AppSettings["Environment"] == "Dev")
-            {
-                StartIIS();
-            }
             BaseUri = new Uri(ConfigurationManager.AppSettings["IdentityServerApplicationUrl"]);
 
             ChromeDriver = new ChromeDriver();
-            ChromeDriver.Manage().Window.Maximize();
+            //ChromeDriver.Manage().Window.Maximize();
             ChromeDriver.Navigate().GoToUrl(BaseUri);
         }
 
